34ba5e0620ab5baf8d6669580c160c6853c1f523
Jan Kozak <galvani78@gmail.com>
2025-04-27 08:57:47 +0200

    restyle and hide the resume from index

--- js/resume.js
+++ js/resume.js
@@ -1,14 +1,34 @@
-$( "h1" ).click(function() {
-    $( "body" ).animate({
+$("h1").click(function () {
+    $("body").animate({
         opacity: 0.25,
         left: "+=50",
         height: "toggle"
-    }, 100, function() {
+    }, 100, function () {
         // Animation complete.
         console.log('complete');
     });
 });
 
-$("body").scroll(function(e){
-    console.log('scroll event',e);
+
+$(document).ready(function () {
+    $.timeliner({
+        timelineContainer: '#timeline',
+        timelineSectionMarker: '.timeline-wrapper',
+        timelineTriggerContainer: '.timeline-series',
+        timelineTriggerAnchor: '.timeline-event a',
+        startState: 'closed',
+        expandAllText: 'Show All',
+        collapseAllText: 'Hide All',
+        startOpen: ['job2023EX', 'job2017EX'],
+        baseSpeed: 200,
+        speed: 4
+    });
+
+    setTimeout(function() {
+        $('#job2023').trigger('click');
+        $('#job2021').trigger('click');
+        $('#job2021_2').trigger('click');
+        $('#job2017').trigger('click');
+    }, 100);
+
 });
